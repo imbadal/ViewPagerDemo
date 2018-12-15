4f3d9115fc0d474be3b5a48baf78f97c4e39ce5e
Badal Kumar <s.k.badal@outlook.com>
2018-12-16 00:11:42 +0530

    release 1.2.1

--- app/src/main/java/com/statusstock/viewpagerdemo/Tab1.java
+++ app/src/main/java/com/statusstock/viewpagerdemo/Tab1.java
@@ -1,19 +1,36 @@
 package com.statusstock.viewpagerdemo;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class Tab1 extends android.support.v4.app.Fragment {
 
+    Dialog dialog;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.tab1, container, false);
+
+        dialog = new Dialog(getContext());
+        dialog.setContentView(R.layout.popup);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        CardView cardView = view.findViewById(R.id.cardView);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
 
         return view;
     }
